b09ae75de37aaf63587ae1de77ae975a9129d062
L-lawliet07 <mandybisht77@gmail.com>
2020-03-08 00:37:53 +0530

    connected to mongo atlas using mongoose

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 /////////////////////////////////////
 // @author : Mandeep Bisht
 /////////////////////////////////////
+
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+const DB = process.env.DB.replace(
+    '<PASSWORD>',
+    process.env.DB_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(connection => {
+    console.log('DB connection is successfull');
+});
 
 const port = process.env.PORT || 3000;
 
