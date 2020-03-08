1b2608eeb8f755c669f0687178ba9dc30c667f9a
L-lawliet07 <mandybisht77@gmail.com>
2020-03-08 19:53:17 +0530

    setup controller prototype

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,33 +1,75 @@
-exports.getCrew = (req, res) => {
+///////////////////////////////////////////////////////////
+// @author : Mandeep Bisht
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+// This function will render home page of the application
+exports.homePage = (req, res) => {
     res
         .status(404)
         .json({
             message: 'this route is under construction'
         });
 };
+///////////////////////////////////////////////////////////
 
 
-exports.createCrew = (req, res) => {
+///////////////////////////////////////////////////////////
+// This function will render all the user page
+exports.userPage = (req, res) => {
     res
         .status(404)
         .json({
-            message: 'This route is under construction'
+            message: 'this route is under construction'
         });
 };
+///////////////////////////////////////////////////////////
 
-exports.getAllShinobi = (req, res) => {
+
+///////////////////////////////////////////////////////////
+// This function will render search Result
+exports.resultPage = (req, res) => {
     res
         .status(404)
         .json({
-            message: 'This route is under construction'
+            message: 'this route is under construction'
         });
 };
+///////////////////////////////////////////////////////////
 
 
-exports.getShinobi = (req, res) => {
-    res
-        .status(404)
-        .json({
-            message: 'This route is under construction'
-        });
-}
+// exports.createCrew = async (req, res) => {
+
+//     try {
+//         const newCrew = await Crew.create(req.body);
+//         res.status(201).json({
+//             status: 'success',
+//             data: {
+//                 crew: newCrew
+//             }
+//         });
+//     } catch (err) {
+//         res.status(400).json({
+//             status: 'fail',
+//             message: err
+//         })
+//     };
+// };
+
+// exports.getAllShinobi = (req, res) => {
+//     res
+//         .status(404)
+//         .json({
+//             message: 'This route is under construction'
+//         });
+// };
+
+
+// exports.getShinobi = (req, res) => {
+//     res
+//         .status(404)
+//         .json({
+//             message: 'This route is under construction'
+//         });
+// }
